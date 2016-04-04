8f4a12e354c1dc11859ae5c54a34f8b88b0c3f1b
Jimmy Shimizu <jimmy@shimizu.se>
2016-04-04 23:03:13 +0200

    Implemented getFavorites and getPlaylists

---
+++ test/unit/prototypes/SonosSystem/getFavorites.js
@@ -0,0 +1,43 @@
+'use strict';
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+require('chai').use(require('sinon-chai'));
+
+context('getFavorites', () => {
+
+  let player;
+  let system;
+  let resultMock = [{}];
+  let success;
+
+  const getFavorites = require('../../../../lib/prototypes/SonosSystem/getFavorites.js');
+
+  describe('When calling getFavorites', () => {
+    before(() => {
+
+
+      player = {
+        browse: sinon.stub().resolves(resultMock)
+      };
+
+      system = {
+        getAnyPlayer: sinon.stub().returns(player)
+      };
+
+      success = sinon.spy();
+
+      return getFavorites.call(system)
+        .then(success);
+    });
+
+    it('Has called browse', () => {
+      expect(player.browse).calledOnce;
+      expect(player.browse.firstCall.args).eql(['FV:2', 0, 0]);
+    });
+
+    it('Returns the expected result', () => {
+      expect(success.firstCall.args[0]).equal(resultMock);
+    });
+  });
+});
---
+++ test/unit/prototypes/SonosSystem/getPlaylists.js
@@ -0,0 +1,99 @@
+'use strict';
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
+require('chai').use(require('sinon-chai'));
+
+context('getPlaylists', () => {
+
+  let system;
+  let resultMock;
+  let success;
+
+  const getPlaylists = require('../../../../lib/prototypes/SonosSystem/getPlaylists.js');
+
+  describe('When calling getPlaylists', () => {
+    let player;
+    before(() => {
+
+      resultMock = {
+        items: [],
+        startIndex: 0,
+        numberReturned: 0,
+        totalMatches: 0
+      };
+
+      player = {
+        browse: sinon.stub().resolves(resultMock)
+      };
+
+      system = {
+        getAnyPlayer: sinon.stub().returns(player)
+      };
+
+      success = sinon.spy();
+
+      return getPlaylists.call(system)
+        .then(success);
+    });
+
+    it('Has called browse', () => {
+      expect(player.browse).calledOnce;
+      expect(player.browse.firstCall.args).eql(['SQ:', 0, 0]);
+    });
+
+    it('Returns the expected result', () => {
+      expect(success.firstCall.args[0]).eql(resultMock);
+    });
+  });
+
+  describe('When result is bigger than maximum chunk', () => {
+    let player;
+    before(() => {
+
+      player = {
+        browse: sinon.stub()
+      };
+
+      player.browse.onCall(0).resolves({
+        items: [1, 2, 3],
+        startIndex: 0,
+        numberReturned: 3,
+        totalMatches: 6
+      });
+
+      player.browse.onCall(1).resolves({
+        items: [4, 5, 6],
+        startIndex: 3,
+        numberReturned: 3,
+        totalMatches: 6
+      });
+
+      // This prevents the loop from continuing without noticing.
+      // Should not happen.
+      player.browse.onCall(2).rejects();
+
+      system = {
+        getAnyPlayer: sinon.stub().returns(player)
+      };
+
+      success = sinon.spy();
+
+      return getPlaylists.call(system)
+        .then(success);
+    });
+
+    it('Should call browse twice', () => {
+      expect(player.browse).calledTwice;
+    });
+
+    it('Has merged result', () => {
+      expect(success.firstCall.args[0]).eql({
+        items: [1, 2, 3, 4, 5, 6],
+        startIndex: 0,
+        numberReturned: 6,
+        totalMatches: 6
+      });
+    });
+  });
+});
