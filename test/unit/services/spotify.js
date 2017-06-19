96e6c947e630d58555bcbb5eff0dcad1b8b919af
Jimmy Shimizu <jimmy@shimizu.se>
2017-06-19 21:55:17 +0200

    Remove Spotify highres for now

--- test/unit/services/spotify.js
+++ test/unit/services/spotify.js
@@ -14,12 +14,4 @@ describe('spotify', function () {
     spotify = servicesContainer[9];
   });
 
-  it('should load highres art', () => {
-
-    return spotify.tryGetHighResArt('x-sonos-spotify:spotify%3atrack%3a3WKg25vrbjJlkhsgl2W4p3?sid=9&flags=8224&sn=9')
-      .then((url) => {
-        expect(url).to.have.string('https://i.scdn.co/image/');
-      });
-  });
-
 });
